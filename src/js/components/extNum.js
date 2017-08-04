99f4c13a3174eee5bf5ccaf70a2fe5bbfcc6a930
Damien Vander Stricht <damien.vanderstricht@ravago.com>
2017-08-04 16:00:12 +0200

    add edit possibilities on owned values

--- src/js/components/extNum.js
+++ src/js/components/extNum.js
@@ -10,36 +10,43 @@ angular.module('coinBalanceApp')
       ctrl.tickMove = '=';
       ctrl.numberShown = '0.00';
       ctrl.symbol = '';
+      ctrl.editable = false;
 
       ctrl.$onChanges = function(changes) {
 
-        var valueThreshold = Math.pow(0.1, ctrl.decimals || 2);
+          var valueThreshold = Math.pow(0.1, ctrl.decimals || 2);
 
-        if (ctrl.number > valueThreshold) {
-          ctrl.globalMove = '+';
-        } else if (ctrl.number < (0 - valueThreshold)) {
-          ctrl.globalMove = '-';
-        }
+          if (ctrl.number > valueThreshold) {
+            ctrl.globalMove = '+';
+          } else if (ctrl.number < (0 - valueThreshold)) {
+            ctrl.globalMove = '-';
+          }
 
-        ctrl.tickMove = '=';
+          ctrl.tickMove = '=';
 
-        if (!changes.number.isFirstChange()) {
-          if (changes.number.currentValue > changes.number.previousValue +
-            valueThreshold) {
-            //            console.log('ticking up : ' + ctrl.number);
-            ctrl.tickMove = '+';
+          if (!changes.number.isFirstChange()) {
+            if (changes.number.currentValue > changes.number.previousValue +
+              valueThreshold) {
+              //            console.log('ticking up : ' + ctrl.number);
+              ctrl.tickMove = '+';
 
-          } else if (changes.number.currentValue < changes.number.previousValue -
-            valueThreshold) {
-            //            console.log('ticking down : ' + ctrl.number);
-            ctrl.tickMove = '-';
+            } else if (changes.number.currentValue < changes.number.previousValue -
+              valueThreshold) {
+              //            console.log('ticking down : ' + ctrl.number);
+              ctrl.tickMove = '-';
+            }
+          }
+
+          ctrl.numberShown = $filter('currency')(ctrl.number, '', ctrl.decimals);
+        },
+        ctrl.onclick = function(e) {
+          if (ctrl.editable) {
+            console.info("edit !");
           }
         }
-
-        ctrl.numberShown = $filter('currency')(ctrl.number, '', ctrl.decimals);
-      }
     },
     bindings: {
+      editable: '<',
       number: '<',
       decimals: '<',
       symbol: '<'
